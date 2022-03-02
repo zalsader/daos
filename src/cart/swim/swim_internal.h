40e954b1e097a124648a37a0fe51123345b011c8
Di Wang <di.wang@intel.com>
2022-03-02 17:21:09 +0000

    DAOS-9856 object: Use higher epoch for migration.
    rebuild/reintegration should use higher epoch during
    recontruction to avoid missing data during incremental
    reintegration.
    
    Only parity related rebuild will use the lower epoch,
    to make sure it will not be higher than those partial
    update epoch.
    
    Signed-off-by: Di Wang <di.wang@intel.com>

--- src/cart/swim/swim_internal.h
+++ src/cart/swim/swim_internal.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016 UChicago Argonne, LLC
- * (C) Copyright 2018-2021 Intel Corporation.
+ * (C) Copyright 2018-2022 Intel Corporation.
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
