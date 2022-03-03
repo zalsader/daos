30dded9fa391ff9b6a620fb6f57aa9ea889e9387
Di Wang <di.wang@intel.com>
2022-03-03 15:58:29 +0000

    DAOS-9636 object: EC parity rotation
    Add dkey hash to EC leader selection process.
    
    Cleanup client shards selection for each operation for EC
    rotation and online OSA.
    
    Remove obj_req_get_tgts(), and move each operation shards
    selection into each dc_obj operation.
    
    Cleanup client stack for different EC related shard selection process.
    
    Signed-off-by: Di Wang <di.wang@intel.com>

--- src/object/obj_enum.c
+++ src/object/obj_enum.c
@@ -13,6 +13,7 @@
 #include <daos_srv/daos_engine.h>
 #include <daos_srv/vos.h>
 #include <daos/object.h>
+#include "obj_internal.h"
 
 static d_iov_t *
 io_csums_iov(struct dss_enum_unpack_io *io)
@@ -974,7 +975,6 @@ out:
  * \a sgls[\a iods_cap].
  *
  * \param[in,out]	io		I/O descriptor
- * \param[in]		oid		oid
  * \param[in]		iods		daos_iod_t array
  * \param[in]		recxs_caps	recxs capacity array
  * \param[in]		sgls		optional sgl array for inline recxs
@@ -984,8 +984,8 @@ out:
  *					\a recxs_caps, \a sgls, and \a ephs
  */
 static void
-dss_enum_unpack_io_init(struct dss_enum_unpack_io *io, daos_unit_oid_t oid,
-			daos_iod_t *iods, int *recxs_caps, d_sg_list_t *sgls,
+dss_enum_unpack_io_init(struct dss_enum_unpack_io *io, daos_iod_t *iods,
+			int *recxs_caps, d_sg_list_t *sgls,
 			daos_epoch_t *akey_ephs, daos_epoch_t *punched_ephs,
 			daos_epoch_t **recx_ephs, int iods_cap)
 {
@@ -1229,6 +1229,9 @@ enum_unpack_key(daos_key_desc_t *kds, char *key_data,
 	if (kds->kd_val_type == OBJ_ITER_DKEY) {
 		if (io->ui_dkey.iov_len == 0) {
 			rc = daos_iov_copy(&io->ui_dkey, &key);
+			if (rc)
+				return rc;
+			io->ui_dkey_hash = obj_dkey2hash(io->ui_oid.id_pub, &key);
 		} else if (!daos_key_match(&io->ui_dkey, &key)) {
 			/* Close current IOD if dkey are different */
 			rc = complete_io(io, cb, cb_arg);
@@ -1238,6 +1241,7 @@ enum_unpack_key(daos_key_desc_t *kds, char *key_data,
 			/* Update to the new dkey */
 			daos_iov_free(&io->ui_dkey);
 			rc = daos_iov_copy(&io->ui_dkey, &key);
+			io->ui_dkey_hash = obj_dkey2hash(io->ui_oid.id_pub, &key);
 		}
 		D_DEBUG(DB_IO, "process dkey "DF_KEY": rc "DF_RC"\n",
 			DP_KEY(&key), DP_RC(rc));
@@ -1550,7 +1554,7 @@ dss_enum_unpack(daos_unit_oid_t oid, daos_key_desc_t *kds, int kds_num,
 		 */
 		csum_iov_in = *csum;
 
-	dss_enum_unpack_io_init(&io, oid, iods, recxs_caps, sgls,
+	dss_enum_unpack_io_init(&io, iods, recxs_caps, sgls,
 				ephs, punched_ephs, recx_ephs,
 				DSS_ENUM_UNPACK_MAX_IODS);
 
